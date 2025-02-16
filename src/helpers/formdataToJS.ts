7588324a50ed510ffc2d236e4cb75a6df23b1428
Ahmed Zahw <ahmed.zahw@vodafone.com>
2025-02-16 04:00:19 +0200

    feat: improve FormData parsing with Lodash for advanced array and nested object handling

--- src/helpers/formdataToJS.ts
+++ src/helpers/formdataToJS.ts
@@ -1,7 +1,23 @@
+import _ from "lodash";
+
 export const formdataToJS = <T extends object>(formData: FormData) => {
-  const obj: Record<string, string | File> = {};
+  const obj: Record<string, any> = {};
+
   formData.forEach((value, key) => {
-    obj[key] = value;
+    // Handle array notation like images[] or files[]
+    if (key.endsWith("[]")) {
+      const cleanKey = key.slice(0, -2);
+
+      if (!obj[cleanKey]) _.set(obj, cleanKey, []);
+
+      obj[cleanKey].push(value);
+
+      return;
+    }
+
+    // Handle normal key-value pairs
+    _.set(obj, key, value);
   });
+
   return obj as T;
 };
